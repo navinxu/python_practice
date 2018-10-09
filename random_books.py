d7428bbd1cf7290482e32cf2948d6f4fb1cc9de1
Navin Xu <everok@126.com>
2018-10-09 20:11:47 +0800

    Init

---
+++ random_books.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+# **********************************************************
+# * Author        : Navin Xu
+# * Email         : admin@navinxu.com
+# * Create time   : 2018-10-09 20:02
+
+# * Filename      : random_books.py
+# * Description   : 
+# **********************************************************
+import random
+
+"""
+一共有《ＰＨＰ圣经》、《Ｌｉｎｕｘ命令行与Ｓｈｅｌｌ脚本编程大全》、《Ｊａｖａｓｃｒｉｐｔ高级程序设计》、《Ｐｙｔｈｏｎ核心编程》与 《嗨翻Ｃ语言》五本书，将随机选择一本书进行学习。
+"""
+
+print(random.choice(["PHP圣经","Linux命令行与Shell脚本编程大全", "Javascript高级程序设计", "Python核心编程", "Head First C"]));
+
+
